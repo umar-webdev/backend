4f02ea085262f05d485550a99b26a7b7ec650e60
Umar Shaikh <63943753+umarkali@users.noreply.github.com>
2023-11-26 14:52:46 +0530

    Api handling and Reponse

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error:", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is runnning at port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO Connection Failedd!!", err);
+  });
 
 /*
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asynceHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asynceHandler };
+
+// const asynceHandler=(fn)=> async ( req, res, next )=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
